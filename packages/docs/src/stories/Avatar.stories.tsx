3a028e16e7ef9592167f88ccfe0bed6ab543aa99
Danrlei <danrleidalfre@gmail.com>
2024-04-22 20:08:04 -0300

    add params to components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/danrleidalfre.png',
     alt: 'Danrlei Dal Fré',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
